ad8d24b822fdc828eaa3a95e40fc2e4cef1952bf
sieunkim <sieunkr@gmail.com>
2020-08-04 00:11:28 +0900

    예제 2-1

---
+++ chapter02/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+    private static final String RESOURCES = "src/main/resources/";
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        //예제 2-1
+        //final Path path = Paths.get(RESOURCES + args[0]);
+        final Path path = Paths.get(RESOURCES + "a.txt");
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+        for (final String line : lines) {
+            final String[] columns = line.split(",");
+            final double amount = Double.parseDouble(columns[1]);
+            total += amount;
+        }
+
+        System.out.println("The total for all transactions is " + total);
+
+    }
+}
